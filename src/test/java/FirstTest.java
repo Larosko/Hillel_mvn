3a53450b1d3c5a1888445063226e74cc4c256450
Dasha <ext-dariya.vengrzhanovskaya@here.com>
2018-03-27 21:06:28 +0300

    + some tests

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,40 +1,59 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import java.io.IOException;
 
 public class FirstTest {
-    public static void main(String[] args) throws IOException {
+    //throws IOException {
         // String[] testData = { "0975156900", "(097)5156900", "(097)515-69-00", "(097)51-56-900", "+380975156900",
         //         "+38 097 515 69 00" };
-
-        test1_isPhone();
-        test2_isPhone();
-    }
+        //test1_isPhone();
+        //test2_isPhone();
+    //}
 
     public static boolean isPhone(String text) {
         String regex = "(?:\\+38)? ?(?:0\\d{2}|\\(0\\d{2}\\)) ?\\d{2}(?:\\d{3}|\\d[- ]\\d{2}[- ]|[ -]\\d{2}[ -]\\d)\\d{2}";
         return text.matches(regex);
     }
 
+    @Test
     public static void test1_isPhone() {
         String testData = "0975156900";
-
         boolean result = isPhone(testData);
-
-        if (result == true) {
-            System.out.println("Test1 passed");
-        } else {
-            System.out.println("Test1 failed");
-        }
+        Assert.assertTrue(result, "Test Number_short");
     }
-
+    @Test
     public static void test2_isPhone() {
         String testData = "asdaddasds";
-
+        //String testData = "0975156900";
         boolean result = isPhone(testData);
-
-        if (result == false) {
-            System.out.println("Test2 passed");
-        } else {
-            System.out.println("Test2 failed");
-        }
+        Assert.assertFalse(result, "Test Abracadabra");
+    }
+    @Test
+    public static void test3_isPhone() {
+        //if special characters exist => fail
+        String testData = "097515690.";
+        boolean result = isPhone(testData);
+        Assert.assertFalse(result, "Test Spec_char");
+    }
+    @Test
+    public static void test4_isPhone() {
+        //if number less then 10 characters => false
+        String testData = "097515690";
+        boolean result = isPhone(testData);
+        Assert.assertFalse(result, "Test Less_10_char");
+    }
+    @Test
+    public static void test5_isPhone() {
+        //if number more then 10 characters => false
+        String testData = "09751569000";
+        boolean result = isPhone(testData);
+        Assert.assertFalse(result, "Test More_10_char");
+    }
+    @Test
+    public static void test6_isPhone() {
+        String testData = "+38 097 51 56-900";
+        boolean result = isPhone(testData);
+        Assert.assertTrue(result, "Test Number_long");
     }
 }
